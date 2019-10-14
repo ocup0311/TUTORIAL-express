412e97d611d23caa251641dc88e05bca08524bd3
OcupHuang <ocup0311@gmail.com>
2019-10-14 17:06:30 +0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 
 // Set up mongoose connection
 const mongoose = require('mongoose')
-const mongoDB = ''
+const mongoDB = process.env.MONGODB_URI || process.env.MONGODB_DEV
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
