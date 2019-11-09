0b0de476b01dc9b51ae1a8d9164dad46c9775a3a
OcupHuang <ocup0311@gmail.com>
2019-11-09 08:47:34 +0800

    debug: keep the date when create author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -66,20 +66,21 @@ exports.author_create_post = [
     // Extract the validation errors from a request.
     const errors = validator.validationResult(req)
 
+    // Create an Author object with escaped and trimmed data.
+    const author = new Author(
+      {
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death
+      })
+
     if (!errors.isEmpty()) {
       // There are errors. Render form again with sanitized values/errors messages.
-      res.render('author_form', { title: '新增作者', author: req.body, errors: errors.array() })
+      res.render('author_form', { title: '新增作者', author: author, errors: errors.array() })
     } else {
       // Data from form is valid.
 
-      // Create an Author object with escaped and trimmed data.
-      const author = new Author(
-        {
-          first_name: req.body.first_name,
-          family_name: req.body.family_name,
-          date_of_birth: req.body.date_of_birth,
-          date_of_death: req.body.date_of_death
-        })
       author.save((err) => {
         if (err) { return next(err) }
         // Successful - redirect to new author record.
