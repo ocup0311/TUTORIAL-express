9121cd1e098cd1453b5e1eaccc2cee073eb5982b
黃家瑋 <huangjiawei@huangjiaweideMacBook-Pro.local>
2021-09-06 22:00:25 +0800

    FIX env

--- configs/index.js
+++ configs/index.js
@@ -1,4 +1,2 @@
 module.exports =
-  process.env.NODE_ENV === 'production' && !process.env.STAGING
-    ? require('./prod')
-    : require('./dev')
+  process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev')
